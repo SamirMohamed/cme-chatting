cc30bd27447ae5822f95b9d5a3eaca1d9bd30c8e
SamirMohamed <samir.mohamed594@gmail.com>
2024-07-01 14:19:15 +0300

    build: fix cassandra loading schema

---
+++ db/init-schema.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start Cassandra in the background
+docker-entrypoint.sh "$@" &
+
+# Wait for Cassandra to start
+until cqlsh -e 'DESC KEYSPACES'; do
+  echo "Waiting for Cassandra to start..."
+  sleep 2
+done
+
+# Import the schema
+cqlsh -f /schema.cql
+
+# Keep the container running
+tail -f /dev/null
